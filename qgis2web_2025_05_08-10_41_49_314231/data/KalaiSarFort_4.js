2cae92b148709972d5ef1774667eb47a3e889754
Christine <christine.spencer.14@ucl.ac.uk>
2025-05-08 10:45:01 +0100

    basemap update commit

---
+++ qgis2web_2025_05_08-10_41_49_314231/data/KalaiSarFort_4.js
@@ -0,0 +1,1 @@
+var json_KalaiSarFort_4 = {"type":"FeatureCollection","name":"KalaiSarFort_4","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"id":null,"name":"Kalai Sar fort"},"geometry":{"type":"Point","coordinates":[68.711986,39.727845]}}]}
